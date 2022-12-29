90dcac7cfc55bfb0e79be06daa15671298ae87a3
vishnutej98 <vishnutej2016@gmail.com>
2022-12-29 15:07:43 +0530

    intial Commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QPushButton, QWidget, QMainWindow, QCheckBox
+
+
+class MainWindow(QMainWindow):
+  def __init__(self):
+    super(MainWindow, self).__init__()
+
+    self.button_is_checked = True
+
+    self.setWindowTitle("App")
+
+    button = QPushButton("Press the Button!")
+    button.setCheckable(True)
+    button.clicked.connect(self.the_button_was_clicked)
+    button.clicked.connect(self.the_button_was_toggled)
+    button.setChecked(self.button_is_checked)
+
+    self.setMinimumSize(QSize(300, 200))
+
+    self.setCentralWidget(button)
+  
+  def the_button_was_clicked(self):
+    print('Clicked!')
+
+  def the_button_was_toggled(self, checked):
+    self.button_is_checked = checked
+
+    print(self.button_is_checked)
+
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
